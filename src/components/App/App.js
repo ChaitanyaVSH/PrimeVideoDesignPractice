595ce0d99954117ab29b73cf5a81d8075931ced4
Chaitanya <kotichai@amazon.com>
2021-01-30 18:57:54 +0530

    Package clean up and setup

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./App.module.css";
+
+const App = () => {
+    return(
+        <div className={styles.App}>
+            <h1>Welcome to My world</h1>
+        </div>
+    );
+}
+
+export default App;
